441ad478d1e7f479fa6993dfe7010de402b45fd6
William Foschiera <william.foschiera@jusbrasil.com.br>
2026-05-08 08:47:15 -0400

    Update Gradient descent optimizer (Math Foundations 1/6)

--- foundations/gradient_descent.py
+++ foundations/gradient_descent.py
@@ -6,7 +6,7 @@ class Solution:
         # Round final answer to 5 decimal places
         x_old = x_new = init
         
-        for i in range(iterations):
+        for _ in range(iterations):
             x_new = x_old - learning_rate * 2 * (x_old)
             x_old = x_new
         return round(x_new, 5)
