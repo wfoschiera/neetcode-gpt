12c109910fe94159808be6280a621dba4b8ce60d
William Foschiera <william.foschiera@jusbrasil.com.br>
2026-05-09 16:53:12 -0400

    Update Activation functions (Math Foundations 2/6)

--- foundations/activations.py
+++ foundations/activations.py
@@ -3,12 +3,12 @@ from numpy.typing import NDArray
 
 
 class Solution:
-    
     def sigmoid(self, z: NDArray[np.float64]) -> NDArray[np.float64]:
         # z is a 1D NumPy array
         # Formula: 1 / (1 + e^(-z))
         # return np.round(your_answer, 5)
-        return np.round(1 / (1 + np.e**(-z)), 5)
+        z = 1 / (1 + np.exp(-z))
+        return np.round(z, 5)
 
     def relu(self, z: NDArray[np.float64]) -> NDArray[np.float64]:
         # z is a 1D NumPy array
