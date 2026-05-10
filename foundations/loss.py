c84f90e2a98242c716cdd026318b5c263480a1eb
William Foschiera <william.foschiera@jusbrasil.com.br>
2026-05-10 10:14:48 -0400

    Update Cross-entropy loss (Math Foundations 4/6)

--- foundations/loss.py
+++ foundations/loss.py
@@ -9,6 +9,7 @@ class Solution:
         self, y_true: NDArray[np.float64], y_pred: NDArray[np.float64]
     ) -> float:
         y_pred = np.clip(y_pred, self.EPSILON, 1 - self.EPSILON)
+        # np.mean already take the sum of elements on a 1D array
         loss = -np.mean(y_true * np.log(y_pred) + (1 - y_true) * np.log(1 - y_pred))
         return round(loss, 4)
 
@@ -16,6 +17,7 @@ class Solution:
         self, y_true: NDArray[np.float64], y_pred: NDArray[np.float64]
     ) -> float:
         y_pred = np.clip(y_pred, self.EPSILON, 1 - self.EPSILON)
+        # on CCE we need to sum over classes before gets the mean value
         loss = -np.mean(np.sum(y_true * np.log(y_pred), axis=1))
 
         return round(loss, 4)
