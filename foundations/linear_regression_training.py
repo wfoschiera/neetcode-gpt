5f44c04637a1937ad92c72f0b8fbb672d3df28d5
William Foschiera <william.foschiera@jusbrasil.com.br>
2026-05-11 15:06:47 -0400

    Update Linear regression training (Math Foundations 6/6)

--- foundations/linear_regression_training.py
+++ foundations/linear_regression_training.py
@@ -24,11 +24,10 @@ class Solution:
         #   2. For each weight index j, compute gradient with get_derivative()
         #   3. Update: weights[j] -= learning_rate * gradient
         # Return np.round(final_weights, 5)
-        weights = np.array(initial_weights)
         for i in range(num_iterations):
-            model_prediction = self.get_model_prediction(X, weights)
-            for j in range(len(weights)):
+            model_prediction = self.get_model_prediction(X, initial_weights)
+            for j in range(len(initial_weights)):
                 gradient = self.get_derivative(model_prediction, Y, len(X), X, j)
-                weights[j] -= self.learning_rate * gradient
+                initial_weights[j] -= self.learning_rate * gradient
 
-        return np.round(weights, 5)
+        return np.round(initial_weights, 5)
